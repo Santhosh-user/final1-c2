b74c13edb04228f7fcf94906cedbc3e94a00d34a
Santhosh Sundararajan <85502994+Santhosh-user@users.noreply.github.com>
2022-04-18 15:41:25 +0530

    Initial commit

---
+++ src/components/ShowStudents.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+export const ShowStudents = () => {
+  const [students, setStudents] = useState([]);
+
+  const disStudents = () => {
+    // Make a request for a user with a given ID
+    axios
+      .get("http://localhost:8080/students")
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  };
+
+  return (
+    <div>
+      <div className="controls">
+        <div>
+          Sort By:{" "}
+          <select
+            // select dropdown needs both value and onChange
+            className="sortby"
+          >
+            <option value="first_name">First Name</option>
+            <option value="gender">Gender</option>
+            <option value="age">Age</option>
+            <option value="tenth_score">10th Score</option>
+            <option value="twelth_score">12th Score</option>
+          </select>
+        </div>
+        <div>
+          Order:
+          <select
+            // select dropdown needs both value and onChange
+            className="sortorder"
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+        <button className="sort">sort</button>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Age</th>
+            <th>10th Score</th>
+            <th>12th Score</th>
+            <th>Branch</th>
+          </tr>
+        </thead>
+        <tbody className="tbody">
+          {/* populate all rows like below: */}
+          <tr className="row">
+            <td className="first_name"></td>
+            <td className="last_name"></td>
+            <td className="email"></td>
+            <td className="gender"></td>
+            <td className="age"></td>
+            <td className="tenth_score"></td>
+            <td className="twelth_score"></td>
+            <td className="preferred_branch"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
